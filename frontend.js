bc57e516074941966e2df091d418c10c6606fa48
Daniel Aguirre <danielaguirre@Daniels-MacBook-Pro.local>
2022-02-05 00:26:23 -0500

    changes from tix 6 into front and server

--- frontend.js
+++ frontend.js
@@ -1,4 +1,4 @@
 function front(){
-    console.log("CHANGES FOR FRONTEND FROM TIX 7");
+    const name = "FROM TIX 6 THIS IS OTHER CHANGE IN FRONT END";
     return null;
 }
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 function someFunc(){
     const server = false;
     console.log("MORE CHANGES FROM TICKET 4!!!");
+    console.log("ALSO ADDED SOME CHANGES INTO SERVER FROM TICKET 6");
     return null;
     // CHANGES FROM SERVER JS IN TICKET 4
 
