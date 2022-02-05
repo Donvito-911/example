0476ea3562922f21401212cf10e739345e41621d
Daniel Aguirre <danielaguirre@Daniels-MacBook-Pro.local>
2022-02-05 00:40:00 -0500

    changes in tix-9

--- frontend.js
+++ frontend.js
@@ -1,4 +1,4 @@
 function front(){
-    const name = "FROM TIX 6 THIS IS OTHER CHANGE IN FRONT END";
+    const name = "FROM TIX 9!!! THIS IS OTHER CHANGE IN FRONT END";
     return null;
 }
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 function someFunc(){
     const server = false;
-    console.log("MORE CHANGES FROM TICKET 4!!!");
+    console.log("MORE CHANGES FROM TICKET 99999!!!");
     console.log("ALSO ADDED SOME CHANGES INTO SERVER FROM TICKET 6");
     return null;
     // CHANGES FROM SERVER JS IN TICKET 4
